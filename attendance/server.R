393d5bc8c926ec17260e1b3483ad6ae2e7662d72
Rachel Severson <rachel.severson@colostate.edu>
2016-09-14 09:27:14 -0600

    remove compute_data function (not using it)

--- attendance/server.R
+++ attendance/server.R
@@ -48,30 +48,7 @@ shinyServer(function(input, output, session) {
     
   })
   
-  
-  compute_data <- function(updateProgress = NULL) {
-    # Create 0-row data frame which will be used to store data
-    dat <- data.frame(x = numeric(0), y = numeric(0))
-    
-    for (i in 1:10) {
-      Sys.sleep(0.25)
-      
-      # Compute new row of data
-      new_row <- data.frame(x = rnorm(1), y = rnorm(1))
-      
-      # If we were passed a progress update function, call it
-      if (is.function(updateProgress)) {
-        text <- paste0("x:", round(new_row$x, 2), " y:", round(new_row$y, 2))
-        updateProgress(detail = text)
-      }
-      
-      # Add the new row of data
-      dat <- rbind(dat, new_row)
-    }
-    
-    dat
-  }
-  
+
       observeEvent(input$download, {
         
         output$message <- renderText({
